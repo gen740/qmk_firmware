12b85b67b5f86a41d22c250ee50201b57f341fa4
gen740 <gen740@gmail.com>
2022-04-15 23:34:43 +0900

    ファームウェア削減のオプションを設定

--- keyboards/crkbd/keymaps/mykeymap/rules.mk
+++ keyboards/crkbd/keymaps/mykeymap/rules.mk
@@ -1,16 +1,17 @@
 OLED_ENABLE = yes
+RGB_MATRIX_ENABLE = yes
 
 MOUSEKEY_ENABLE = no
-
 TAP_DANCE_ENABLE = no
 UNICODE_ENABLE = no
-
-
-RGB_MATRIX_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+SPACE_CADET_ENABLE = no
+MAGIC_ENABLE = no
 
 SRC += naginata_v13u.c
 
-EXTRAFLAGS += -flto
+# EXTRAFLAGS += -flto
 LTO_ENABLE = yes
 
 # check no when firmware is right
