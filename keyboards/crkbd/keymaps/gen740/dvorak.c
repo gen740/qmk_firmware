1272dcc1152353d81f9f9135c7a04890b266ff63
gen740 <54583542+gen740@users.noreply.github.com>
2024-10-13 14:04:47 +0900

    feat(keymap): Update keymaps

--- keyboards/crkbd/keymaps/gen740/dvorak.c
+++ keyboards/crkbd/keymaps/gen740/dvorak.c
@@ -1,21 +1,20 @@
+#include "dvorak.h"
 #include "dvorak_keydata.h"
 
-uint64_t dvorak_keycomb      = 0;
-uint64_t prev_dvorak_keycomb = 0;
+uint64_t dvorak_keycomb          = 0;
+bool     dv_combo_char_activated = false;
 
-void dv_press_key(uint32_t key) {
-    prev_dvorak_keycomb = dvorak_keycomb;
+void dv_press_key(uint64_t key) {
     dvorak_keycomb |= 1UL << (key - DV_A);
 }
 
-void dv_release_key(uint32_t key) {
-    prev_dvorak_keycomb = dvorak_keycomb;
+void dv_release_key(uint64_t key) {
     dvorak_keycomb &= ~(1UL << (key - DV_A));
 }
 
-const char* dvorak_search(uint32_t key_comb) {
+const char* dvorak_search(uint64_t key_comb) {
     for (int i = 0; i < DVORAK_KEY_NUMBER; i++) {
-        if (dvmap[i].key == 0xffffffff) {
+        if (dvmap[i].key == 0xffffffff) { // End of dvmap
             break;
         }
         if (key_comb == dvmap[i].key) {
@@ -25,48 +24,116 @@ const char* dvorak_search(uint32_t key_comb) {
     return NULL;
 }
 
-static uint32_t emit_event_callback(uint32_t trigger_time, void* cb_arg) {
-    const char* str = dvorak_search(dvorak_keycomb);
-    if (str != NULL) {
-        if (strcmp(str, "nag") == 0) {
-            tap_code(KC_LNG1);
-            layer_move(L_NAG);
-        } else {
-            send_string(str);
-        }
-        prev_dvorak_keycomb = 0;
-        dvorak_keycomb      = 0;
-    } else {
-        str = dvorak_search(prev_dvorak_keycomb);
-        if (strcmp(str, "nag") == 0) {
-            tap_code(KC_LNG1);
-            layer_move(L_NAG);
-        } else {
-            send_string(str);
-        }
-        dvorak_keycomb = (dvorak_keycomb ^ prev_dvorak_keycomb) & dvorak_keycomb;
-    }
-    return 0;
-}
-
-deferred_token emit_event_callback_token = 0;
-
 bool process_dvorak(uint16_t keycode, keyrecord_t* record) {
-    switch (keycode) {
-        case DV_A ... DV_RSFT:
-            if (record->event.pressed) {
+    if (record->event.pressed) {
+        switch (keycode) {
+            case DV_A ... DV_RSFT:
                 dv_press_key(keycode);
-                if (!extend_deferred_exec(emit_event_callback_token, DVORAK_COMBO_DURATION)) {
-                    emit_event_callback_token = defer_exec(DVORAK_COMBO_DURATION, emit_event_callback, NULL);
+                dv_combo_char_activated = false;
+                break;
+            default:
+                break;
+        }
+    } else {
+        switch (keycode) {
+            case DV_A ... DV_RSFT: {
+                if (dv_combo_char_activated) {
+                    dv_release_key(keycode);
+                    break;
                 }
-            } else {
-                emit_event_callback(0, NULL);
-                cancel_deferred_exec(emit_event_callback_token);
+                const char* str = dvorak_search(dvorak_keycomb);
+                if (str != NULL) {
+                    dv_combo_char_activated = true;
+                    if (strcmp(str, "nag") == 0) {
+                        tap_code(KC_LNG1);
+                        layer_move(L_NAG);
+                    } else {
+                        send_string(str);
+                    }
+                } else {
+                    str = dvorak_search(1UL << (keycode - DV_A));
+                    if (str != NULL) {
+                        if (strcmp(str, "nag") == 0) {
+                            tap_code(KC_LNG1);
+                            layer_move(L_NAG);
+                        } else {
+                            send_string(str);
+                        }
+                    }
+                }
                 dv_release_key(keycode);
+                return true;
+                break;
             }
-            break;
-        default:
-            break;
+            default:
+                break;
+        }
     }
     return true;
 }
+
+/**/
+/*const PROGMEM int dvorak_persistent_keys[2] = {*/
+/*    DV_LSFT,*/
+/*    DV_RSFT,*/
+/*};*/
+/**/
+/*uint64_t dvorak_keycomb      = 0;*/
+/*uint64_t prev_dvorak_keycomb = 0;*/
+/**/
+/*void dv_press_key(uint32_t key) {*/
+/*    prev_dvorak_keycomb = dvorak_keycomb;*/
+/*    dvorak_keycomb |= 1UL << (key - DV_A);*/
+/*}*/
+/**/
+/*void dv_release_key(uint32_t key) {*/
+/*    prev_dvorak_keycomb = dvorak_keycomb;*/
+/*    dvorak_keycomb &= ~(1UL << (key - DV_A));*/
+/*}*/
+/**/
+/**/
+/*static uint32_t emit_event_callback(uint32_t trigger_time, void* cb_arg) {*/
+/*    const char* str = dvorak_search(dvorak_keycomb);*/
+/*    if (str != NULL) {*/
+/*        if (strcmp(str, "nag") == 0) {*/
+/*            tap_code(KC_LNG1);*/
+/*            layer_move(L_NAG);*/
+/*        } else {*/
+/*            send_string(str);*/
+/*        }*/
+/*        prev_dvorak_keycomb = 0;*/
+/*        dvorak_keycomb      = 0;*/
+/*    } else {*/
+/*        str = dvorak_search(prev_dvorak_keycomb);*/
+/*        if (strcmp(str, "nag") == 0) {*/
+/*            tap_code(KC_LNG1);*/
+/*            layer_move(L_NAG);*/
+/*        } else {*/
+/*            send_string(str);*/
+/*        }*/
+/*        dvorak_keycomb = (dvorak_keycomb ^ prev_dvorak_keycomb) & dvorak_keycomb;*/
+/*    }*/
+/*    return 0;*/
+/*}*/
+/**/
+/*deferred_token emit_event_callback_token = 0;*/
+/**/
+/*bool process_dvorak(uint16_t keycode, keyrecord_t* record) {*/
+/*    switch (keycode) {*/
+/*        case DV_A ... DV_RSFT:*/
+/*            if (record->event.pressed) {*/
+/*                dv_press_key(keycode);*/
+/*                if (!extend_deferred_exec(emit_event_callback_token, DVORAK_COMBO_DURATION)) {*/
+/*                    emit_event_callback_token = defer_exec(DVORAK_COMBO_DURATION, emit_event_callback, NULL);*/
+/*                }*/
+/*            } else {*/
+/*                emit_event_callback(0, NULL);*/
+/*                cancel_deferred_exec(emit_event_callback_token);*/
+/*                dv_release_key(keycode);*/
+/*            }*/
+/*            break;*/
+/*        default:*/
+/*            break;*/
+/*    }*/
+/*    return true;*/
+/*}*/
--- keyboards/crkbd/keymaps/gen740/dvorak_keydata.c
+++ keyboards/crkbd/keymaps/gen740/dvorak_keydata.c
@@ -34,6 +34,7 @@
 #define B_LSFT (1UL << (DV_LSFT - DV_A))
 #define B_RSFT (1UL << (DV_RSFT - DV_A))
 
+
 const PROGMEM dvorak_keymap dvmap[DVORAK_KEY_NUMBER] = {
     // Normal keys
     {.key = B_QUOT, .val = "'"},
@@ -73,7 +74,7 @@ const PROGMEM dvorak_keymap dvmap[DVORAK_KEY_NUMBER] = {
     {.key = B_Z, .val = "z"},
 
     {.key = B_LSFT, .val = " "},
-    {.key = B_RSFT, .val = " "},
+    {.key = B_RSFT, .val = SS_TAP(X_ENT)},
 
     // Shift keys
     {.key = B_RSFT | B_QUOT, .val = "\""},
@@ -93,7 +94,6 @@ const PROGMEM dvorak_keymap dvmap[DVORAK_KEY_NUMBER] = {
     {.key = B_RSFT | B_E, .val = "E"},
     {.key = B_RSFT | B_U, .val = "U"},
     {.key = B_RSFT | B_I, .val = "I"},
-
 
     {.key = B_RSFT | B_D, .val = "D"},
     {.key = B_RSFT | B_H, .val = "H"},
@@ -116,126 +116,135 @@ const PROGMEM dvorak_keymap dvmap[DVORAK_KEY_NUMBER] = {
     // Ctrl keys
     {.key = B_LSFT | B_QUOT, .val = SS_LCTL("'")},
     {.key = B_LSFT | B_COMM, .val = SS_LCTL(",")},
-    {.key = B_LSFT | B_DOT, .val  = SS_LCTL(".")},
-    {.key = B_LSFT | B_P, .val    = SS_LCTL("p")},
-    {.key = B_LSFT | B_Y, .val    = SS_LCTL("y")},
+    {.key = B_LSFT | B_DOT, .val = SS_LCTL(".")},
+    {.key = B_LSFT | B_P, .val = SS_LCTL("p")},
+    {.key = B_LSFT | B_Y, .val = SS_LCTL("y")},
 
-    {.key = B_LSFT | B_F, .val    = SS_LCTL("f")},
-    {.key = B_LSFT | B_G, .val    = SS_LCTL("g")},
-    {.key = B_LSFT | B_C, .val    = SS_LCTL("c")},
-    {.key = B_LSFT | B_R, .val    = SS_LCTL("r")},
-    {.key = B_LSFT | B_L, .val    = SS_LCTL("l")},
+    {.key = B_LSFT | B_F, .val = SS_LCTL("f")},
+    {.key = B_LSFT | B_G, .val = SS_LCTL("g")},
+    {.key = B_LSFT | B_C, .val = SS_LCTL("c")},
+    {.key = B_LSFT | B_R, .val = SS_LCTL("r")},
+    {.key = B_LSFT | B_L, .val = SS_LCTL("l")},
 
-    {.key = B_LSFT | B_A, .val    = SS_LCTL("a")},
-    {.key = B_LSFT | B_O, .val    = SS_LCTL("o")},
-    {.key = B_LSFT | B_E, .val    = SS_LCTL("e")},
-    {.key = B_LSFT | B_U, .val    = SS_LCTL("u")},
-    {.key = B_LSFT | B_I, .val    = SS_LCTL("i")},
+    {.key = B_LSFT | B_A, .val = SS_LCTL("a")},
+    {.key = B_LSFT | B_O, .val = SS_LCTL("o")},
+    {.key = B_LSFT | B_E, .val = SS_LCTL("e")},
+    {.key = B_LSFT | B_U, .val = SS_LCTL("u")},
+    {.key = B_LSFT | B_I, .val = SS_LCTL("i")},
 
-    {.key = B_LSFT | B_D, .val    = SS_LCTL("d")},
-    {.key = B_LSFT | B_H, .val    = SS_LCTL("h")},
-    {.key = B_LSFT | B_T, .val    = SS_LCTL("t")},
-    {.key = B_LSFT | B_N, .val    = SS_LCTL("n")},
-    {.key = B_LSFT | B_S, .val    = SS_LCTL("s")},
+    {.key = B_LSFT | B_D, .val = SS_LCTL("d")},
+    {.key = B_LSFT | B_H, .val = SS_LCTL("h")},
+    {.key = B_LSFT | B_T, .val = SS_LCTL("t")},
+    {.key = B_LSFT | B_N, .val = SS_LCTL("n")},
+    {.key = B_LSFT | B_S, .val = SS_LCTL("s")},
 
     {.key = B_LSFT | B_SCLN, .val = SS_LCTL(";")},
-    {.key = B_LSFT | B_Q, .val    = SS_LCTL("q")},
-    {.key = B_LSFT | B_J, .val    = SS_LCTL("j")},
-    {.key = B_LSFT | B_K, .val    = SS_LCTL("k")},
-    {.key = B_LSFT | B_X, .val    = SS_LCTL("x")},
+    {.key = B_LSFT | B_Q, .val = SS_LCTL("q")},
+    {.key = B_LSFT | B_J, .val = SS_LCTL("j")},
+    {.key = B_LSFT | B_K, .val = SS_LCTL("k")},
+    {.key = B_LSFT | B_X, .val = SS_LCTL("x")},
 
-    {.key = B_LSFT | B_B, .val    = SS_LCTL("b")},
-    {.key = B_LSFT | B_M, .val    = SS_LCTL("m")},
-    {.key = B_LSFT | B_W, .val    = SS_LCTL("w")},
-    {.key = B_LSFT | B_V, .val    = SS_LCTL("v")},
-    {.key = B_LSFT | B_Z, .val    = SS_LCTL("z")},
+    {.key = B_LSFT | B_B, .val = SS_LCTL("b")},
+    {.key = B_LSFT | B_M, .val = SS_LCTL("m")},
+    {.key = B_LSFT | B_W, .val = SS_LCTL("w")},
+    {.key = B_LSFT | B_V, .val = SS_LCTL("v")},
+    {.key = B_LSFT | B_Z, .val = SS_LCTL("z")},
+
+    // Ctrl + Special keys
+    {.key = B_LSFT | B_E | B_N, .val = SS_LCTL("^")},
+    {.key = B_LSFT | B_J | B_W, .val = SS_LCTL(SS_TAP(X_TAB))},
 
     // Ctrl + Shift keys
     {.key = B_LSFT | B_RSFT | B_QUOT, .val = SS_LCTL(SS_LSFT("'"))},
     {.key = B_LSFT | B_RSFT | B_COMM, .val = SS_LCTL(SS_LSFT(","))},
-    {.key = B_LSFT | B_RSFT | B_DOT, .val  = SS_LCTL(SS_LSFT("."))},
-    {.key = B_LSFT | B_RSFT | B_P, .val    = SS_LCTL(SS_LSFT("p"))},
-    {.key = B_LSFT | B_RSFT | B_Y, .val    = SS_LCTL(SS_LSFT("y"))},
+    {.key = B_LSFT | B_RSFT | B_DOT, .val = SS_LCTL(SS_LSFT("."))},
+    {.key = B_LSFT | B_RSFT | B_P, .val = SS_LCTL(SS_LSFT("p"))},
+    {.key = B_LSFT | B_RSFT | B_Y, .val = SS_LCTL(SS_LSFT("y"))},
 
-    {.key = B_LSFT | B_RSFT | B_F, .val    = SS_LCTL(SS_LSFT("f"))},
-    {.key = B_LSFT | B_RSFT | B_G, .val    = SS_LCTL(SS_LSFT("g"))},
-    {.key = B_LSFT | B_RSFT | B_C, .val    = SS_LCTL(SS_LSFT("c"))},
-    {.key = B_LSFT | B_RSFT | B_R, .val    = SS_LCTL(SS_LSFT("r"))},
-    {.key = B_LSFT | B_RSFT | B_L, .val    = SS_LCTL(SS_LSFT("l"))},
+    {.key = B_LSFT | B_RSFT | B_F, .val = SS_LCTL(SS_LSFT("f"))},
+    {.key = B_LSFT | B_RSFT | B_G, .val = SS_LCTL(SS_LSFT("g"))},
+    {.key = B_LSFT | B_RSFT | B_C, .val = SS_LCTL(SS_LSFT("c"))},
+    {.key = B_LSFT | B_RSFT | B_R, .val = SS_LCTL(SS_LSFT("r"))},
+    {.key = B_LSFT | B_RSFT | B_L, .val = SS_LCTL(SS_LSFT("l"))},
 
-    {.key = B_LSFT | B_RSFT | B_A, .val    = SS_LCTL(SS_LSFT("a"))},
-    {.key = B_LSFT | B_RSFT | B_O, .val    = SS_LCTL(SS_LSFT("o"))},
-    {.key = B_LSFT | B_RSFT | B_E, .val    = SS_LCTL(SS_LSFT("e"))},
-    {.key = B_LSFT | B_RSFT | B_U, .val    = SS_LCTL(SS_LSFT("u"))},
-    {.key = B_LSFT | B_RSFT | B_I, .val    = SS_LCTL(SS_LSFT("i"))},
+    {.key = B_LSFT | B_RSFT | B_A, .val = SS_LCTL(SS_LSFT("a"))},
+    {.key = B_LSFT | B_RSFT | B_O, .val = SS_LCTL(SS_LSFT("o"))},
+    {.key = B_LSFT | B_RSFT | B_E, .val = SS_LCTL(SS_LSFT("e"))},
+    {.key = B_LSFT | B_RSFT | B_U, .val = SS_LCTL(SS_LSFT("u"))},
+    {.key = B_LSFT | B_RSFT | B_I, .val = SS_LCTL(SS_LSFT("i"))},
 
-    {.key = B_LSFT | B_RSFT | B_D, .val    = SS_LCTL(SS_LSFT("d"))},
-    {.key = B_LSFT | B_RSFT | B_H, .val    = SS_LCTL(SS_LSFT("h"))},
-    {.key = B_LSFT | B_RSFT | B_T, .val    = SS_LCTL(SS_LSFT("t"))},
-    {.key = B_LSFT | B_RSFT | B_N, .val    = SS_LCTL(SS_LSFT("n"))},
-    {.key = B_LSFT | B_RSFT | B_S, .val    = SS_LCTL(SS_LSFT("s"))},
+    {.key = B_LSFT | B_RSFT | B_D, .val = SS_LCTL(SS_LSFT("d"))},
+    {.key = B_LSFT | B_RSFT | B_H, .val = SS_LCTL(SS_LSFT("h"))},
+    {.key = B_LSFT | B_RSFT | B_T, .val = SS_LCTL(SS_LSFT("t"))},
+    {.key = B_LSFT | B_RSFT | B_N, .val = SS_LCTL(SS_LSFT("n"))},
+    {.key = B_LSFT | B_RSFT | B_S, .val = SS_LCTL(SS_LSFT("s"))},
 
     {.key = B_LSFT | B_RSFT | B_SCLN, .val = SS_LCTL(SS_LSFT(";"))},
-    {.key = B_LSFT | B_RSFT | B_Q, .val    = SS_LCTL(SS_LSFT("q"))},
-    {.key = B_LSFT | B_RSFT | B_J, .val    = SS_LCTL(SS_LSFT("j"))},
-    {.key = B_LSFT | B_RSFT | B_K, .val    = SS_LCTL(SS_LSFT("k"))},
-    {.key = B_LSFT | B_RSFT | B_X, .val    = SS_LCTL(SS_LSFT("x"))},
+    {.key = B_LSFT | B_RSFT | B_Q, .val = SS_LCTL(SS_LSFT("q"))},
+    {.key = B_LSFT | B_RSFT | B_J, .val = SS_LCTL(SS_LSFT("j"))},
+    {.key = B_LSFT | B_RSFT | B_K, .val = SS_LCTL(SS_LSFT("k"))},
+    {.key = B_LSFT | B_RSFT | B_X, .val = SS_LCTL(SS_LSFT("x"))},
 
-    {.key = B_LSFT | B_RSFT | B_B, .val    = SS_LCTL(SS_LSFT("b"))},
-    {.key = B_LSFT | B_RSFT | B_M, .val    = SS_LCTL(SS_LSFT("m"))},
-    {.key = B_LSFT | B_RSFT | B_W, .val    = SS_LCTL(SS_LSFT("w"))},
-    {.key = B_LSFT | B_RSFT | B_V, .val    = SS_LCTL(SS_LSFT("v"))},
-    {.key = B_LSFT | B_RSFT | B_Z, .val    = SS_LCTL(SS_LSFT("z"))},
+    {.key = B_LSFT | B_RSFT | B_B, .val = SS_LCTL(SS_LSFT("b"))},
+    {.key = B_LSFT | B_RSFT | B_M, .val = SS_LCTL(SS_LSFT("m"))},
+    {.key = B_LSFT | B_RSFT | B_W, .val = SS_LCTL(SS_LSFT("w"))},
+    {.key = B_LSFT | B_RSFT | B_V, .val = SS_LCTL(SS_LSFT("v"))},
+    {.key = B_LSFT | B_RSFT | B_Z, .val = SS_LCTL(SS_LSFT("z"))},
 
     // Symbol keys
-    {.key = B_T | B_QUOT, .val = "`"},
-    {.key = B_T | B_COMM, .val = "%"},
-    {.key = B_T | B_DOT, .val = "{"},
-    {.key = B_T | B_P, .val = "("},
-    {.key = B_T | B_Y, .val = "["},
+    {.key = B_H | B_T | B_QUOT, .val = "`"},
+    {.key = B_H | B_T | B_COMM, .val = "%"},
+    {.key = B_H | B_T | B_DOT, .val = "{"},
+    {.key = B_H | B_T | B_P, .val = "("},
+    {.key = B_H | B_T | B_Y, .val = "["},
 
-    {.key = B_E | B_F, .val = "]"},
-    {.key = B_E | B_G, .val = ")"},
-    {.key = B_E | B_C, .val = "}"},
-    {.key = B_E | B_R, .val = "$"},
-    {.key = B_E | B_L, .val = "!"},
+    {.key = B_E | B_U | B_F, .val = "]"},
+    {.key = B_E | B_U | B_G, .val = ")"},
+    {.key = B_E | B_U | B_C, .val = "}"},
+    {.key = B_E | B_U | B_R, .val = "$"},
+    {.key = B_E | B_U | B_L, .val = "!"},
 
-    {.key = B_T | B_A, .val = "#"},
-    {.key = B_T | B_O, .val = "/"},
-    /*{.key = B_T | B_E, .val = "" },*/
-    {.key = B_T | B_U, .val = "+"},
-    {.key = B_T | B_I, .val = "|"},
+    {.key = B_H | B_T | B_A, .val = "#"},
+    {.key = B_H | B_T | B_O, .val = "/"},
+    {.key = B_H | B_T | B_E, .val = "\\" },
+    {.key = B_H | B_T | B_U, .val = "+"},
+    {.key = B_H | B_T | B_I, .val = "|"},
 
-    {.key = B_E | B_D, .val = "_"},
-    {.key = B_E | B_H, .val = "-"},
-    /*{.key = B_T | B_E, .val = "" },*/
-    {.key = B_E | B_N, .val = "^"},
-    {.key = B_E | B_S, .val = "~"},
+    {.key = B_E | B_U | B_D, .val = "_"},
+    {.key = B_E | B_U | B_H, .val = "-"},
+    {.key = B_E | B_U | B_T, .val = "*" },
+    {.key = B_E | B_U | B_N, .val = "^"},
+    {.key = B_E | B_U | B_S, .val = "~"},
 
-    /*{.key = B_T | B_SCLN, .val = "" },*/
-    {.key = B_T | B_Q, .val = "\\"},
-    /*{.key = B_T | B_J, .val = "="},*/
-    {.key = B_T | B_K, .val = "&"},
-    {.key = B_T | B_X, .val = "=" },
+    {.key = B_H | B_T | B_SCLN, .val = "" },
+    {.key = B_H | B_T | B_Q, .val = ""},
+    {.key = B_H | B_T | B_J, .val = "&"},
+    {.key = B_H | B_T | B_K, .val = "="},
+    {.key = B_H | B_T | B_X, .val = ""},
 
-    {.key = B_E | B_B, .val = "*" },
-    {.key = B_E | B_M, .val = "@"},
-    /*{.key = B_T | B_W, .val = ""},*/
-    /*{.key = B_E | B_V, .val = "" },*/
-    /*{.key = B_E | B_Z, .val = "" },*/
+    /*{.key = B_E | B_U | B_B, .val = ""},*/
+    {.key = B_E | B_U | B_M, .val = "@"},
+    {.key = B_E | B_U | B_W, .val = "?"},
+    /*{.key = B_E | B_U | B_V, .val = ""},*/
+    /*{.key = B_E | B_U | B_Z, .val = "" },*/
 
-    {.key = B_W | B_A, .val = "1"},
-    {.key = B_W | B_O, .val = "2"},
-    {.key = B_W | B_E, .val = "3"},
-    {.key = B_W | B_U, .val = "4"},
-    {.key = B_W | B_I, .val = "5"},
+    {.key = B_T | B_N | B_QUOT, .val = ""},
+    {.key = B_T | B_N | B_COMM, .val = "1"},
+    {.key = B_T | B_N | B_DOT, .val = "2"},
+    {.key = B_T | B_N | B_P, .val = "3"},
+    {.key = B_T | B_N | B_Y, .val = ""},
 
-    {.key = B_J | B_D, .val = "6"},
-    {.key = B_J | B_H, .val = "7"},
-    {.key = B_J | B_T, .val = "8"},
-    {.key = B_J | B_N, .val = "9"},
-    {.key = B_J | B_S, .val = "0"},
+    {.key = B_T | B_N | B_A, .val = ""},
+    {.key = B_T | B_N | B_O, .val = "4"},
+    {.key = B_T | B_N | B_E, .val = "5"},
+    {.key = B_T | B_N | B_U, .val = "6"},
+    {.key = B_T | B_N | B_I, .val = "0"},
 
+    {.key = B_T | B_N | B_SCLN, .val = ""},
+    {.key = B_T | B_N | B_Q, .val = "7"},
+    {.key = B_T | B_N | B_J, .val = "8"},
+    {.key = B_T | B_N | B_K, .val = "9"},
+    {.key = B_T | B_N | B_X, .val = ""},
 
     // special keys
     {.key = B_U | B_I, .val = SS_TAP(X_ESC)}, // escape
@@ -244,7 +253,6 @@ const PROGMEM dvorak_keymap dvmap[DVORAK_KEY_NUMBER] = {
 
     /*{.key = B_H | B_N, .val = "hypr"},*/
     /*{.key = B_U | B_O, .val = "hypr"},*/
-
 
     /*{.key = B_H | B_A ,.val = "A"},*/
     /*{.key = B_H | B_B ,.val = "B"},*/
