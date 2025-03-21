c232782e2e4fe67d4df1e811c4a5b2514637db5a
gen740 <54583542+gen740@users.noreply.github.com>
2025-03-21 22:37:48 +0900

    Update

--- keyboards/crkbd/keymaps/gen740/dvorak.c
+++ keyboards/crkbd/keymaps/gen740/dvorak.c
@@ -1,66 +1,71 @@
-#include "gen740.h"
 #include "dvorak.h"
+
+#include <string.h>
+
+#include "action.h"
+#include "action_layer.h"
 #include "dvorak_keydata.h"
+#include "gen740.h"
 
 const dvorak_node_t* dv_current_node = &dvorak_node_root;
 
-void                 dv_send_string(const char* str) {
-    if (strcmp(str, "nag") == 0) {
-        tap_code(KC_LNG1);
-        layer_move(L_NAG);
-        return;
-    }
-    send_string(str);
+void dv_send_string(const char* str) {
+  if (strcmp(str, "nag") == 0) {
+    tap_code(KC_LNG1);
+    layer_move(L_NAG);
+    return;
+  }
+  send_string(str);
 }
 
 bool process_dvorak(uint16_t keycode, keyrecord_t* record) {
-    switch (keycode) {
-        case DV_A ... DV_RSFT3: {
-            if (record->event.pressed) {
-                const dvorak_node_t* next_node = dv_current_node->next_node(keycode);
+  switch (keycode) {
+    case DV_A ... DV_RSFT3: {
+      if (record->event.pressed) {
+        const dvorak_node_t* next_node = dv_current_node->next_node(keycode);
 
-                // if not found, rollback
-                if (next_node == NULL) {
-                    dv_send_string(dv_current_node->value);
+        // if not found, rollback
+        if (next_node == NULL) {
+          dv_send_string(dv_current_node->value);
 
-                    uint16_t keybuf[8];
-                    uint8_t  keybuf_len = 0;
+          uint16_t keybuf[8];
+          uint8_t keybuf_len = 0;
 
-                    while (dv_current_node->parent != NULL) {
-                        dv_current_node        = dv_current_node->parent;
-                        const dvorak_node_t* n = dv_current_node->next_node(keycode);
-                        if (n != NULL) {
-                            dv_current_node = n;
-                            break;
-                        }
-                        keybuf[keybuf_len++] = dv_current_node->key;
-                    }
-
-                    for (int8_t i = keybuf_len - 1; i >= 0; i--) {
-                        const dvorak_node_t* n = dv_current_node->next_node(keybuf[i]);
-                        if (n == NULL) {
-                            break;
-                        } else {
-                            dv_current_node = n;
-                        }
-                    }
-
-                } else {
-                    dv_current_node = next_node;
-                }
-            } else {
-                if (dv_current_node->prev_node(keycode) != NULL) {
-                    dv_send_string(dv_current_node->value);
-                }
-                const dvorak_node_t* n = dv_current_node->prev_node(keycode);
-                if (n != NULL) {
-                    dv_current_node = n;
-                }
+          while (dv_current_node->parent != NULL) {
+            dv_current_node = dv_current_node->parent;
+            const dvorak_node_t* n = dv_current_node->next_node(keycode);
+            if (n != NULL) {
+              dv_current_node = n;
+              break;
             }
-            return false;
+            keybuf[keybuf_len++] = dv_current_node->key;
+          }
+
+          for (int8_t i = keybuf_len - 1; i >= 0; i--) {
+            const dvorak_node_t* n = dv_current_node->next_node(keybuf[i]);
+            if (n == NULL) {
+              break;
+            } else {
+              dv_current_node = n;
+            }
+          }
+
+        } else {
+          dv_current_node = next_node;
         }
-        default:
-            break;
+      } else {
+        if (dv_current_node->prev_node(keycode) != NULL) {
+          dv_send_string(dv_current_node->value);
+        }
+        const dvorak_node_t* n = dv_current_node->prev_node(keycode);
+        if (n != NULL) {
+          dv_current_node = n;
+        }
+      }
+      return false;
     }
-    return true;
+    default:
+      break;
+  }
+  return true;
 }
--- keyboards/crkbd/keymaps/gen740/naginata.c
+++ keyboards/crkbd/keymaps/gen740/naginata.c
@@ -1,66 +1,86 @@
-#include "gen740.h"
 #include "naginata.h"
+
+#include <string.h>
+
+#include "action.h"
+#include "action_layer.h"
+#include "gen740.h"
 #include "naginata_keydata.h"
 
 const naginata_node_t* ng_current_node = &naginata_node_root;
 
-void                   ng_send_string(const char* str) {
-    if (strcmp(str, "dvo") == 0) {
-        tap_code(KC_LNG2);
-        layer_move(L_DVO);
-        return;
-    }
-    send_string(str);
+char buffer[16];
+size_t buffer_index = 0;
+
+const int ng_send_string_delay = 1;
+
+uint32_t delayed_send_char(uint32_t trigger_time, void* cb_arg) {
+  if (buffer[buffer_index] == '\0' || buffer_index >= 16) {
+    buffer_index = 0;
+    return 0;
+  } else {
+    send_char(buffer[buffer_index++]);
+    return ng_send_string_delay;
+  }
+}
+
+void ng_send_string(const char* str) {
+  if (strcmp(str, "dvo") == 0) {
+    tap_code(KC_LNG2);
+    layer_move(L_DVO);
+    return;
+  }
+  send_string(str);
 }
 
 bool process_naginata(uint16_t keycode, keyrecord_t* record) {
-    switch (keycode) {
-        case NG_A ... NG_RSFT3: {
-            if (record->event.pressed) {
-                const naginata_node_t* next_node = ng_current_node->next_node(keycode);
+  switch (keycode) {
+    case NG_A ... NG_RSFT3: {
+      if (record->event.pressed) {
+        const naginata_node_t* next_node = ng_current_node->next_node(keycode);
 
-                // if not found, rollback
-                if (next_node == NULL) {
-                    ng_send_string(ng_current_node->value);
+        // if not found, rollback
+        if (next_node == NULL) {
+          ng_send_string(ng_current_node->value);
 
-                    uint16_t keybuf[8];
-                    uint8_t  keybuf_len = 0;
+          uint16_t keybuf[8];
+          uint8_t keybuf_len = 0;
 
-                    while (ng_current_node->parent != NULL) {
-                        ng_current_node          = ng_current_node->parent;
-                        const naginata_node_t* n = ng_current_node->next_node(keycode);
-                        if (n != NULL) {
-                            ng_current_node = n;
-                            break;
-                        }
-                        keybuf[keybuf_len++] = ng_current_node->key;
-                    }
-
-                    for (int8_t i = keybuf_len - 1; i >= 0; i--) {
-                        const naginata_node_t* n = ng_current_node->next_node(keybuf[i]);
-                        if (n == NULL) {
-                            break;
-                        } else {
-                            ng_current_node = n;
-                        }
-                    }
-
-                } else {
-                    ng_current_node = next_node;
-                }
-            } else {
-                if (ng_current_node->prev_node(keycode) != NULL) {
-                    ng_send_string(ng_current_node->value);
-                }
-                const naginata_node_t* n = ng_current_node->prev_node(keycode);
-                if (n != NULL) {
-                    ng_current_node = n;
-                }
+          while (ng_current_node->parent != NULL) {
+            ng_current_node = ng_current_node->parent;
+            const naginata_node_t* n = ng_current_node->next_node(keycode);
+            if (n != NULL) {
+              ng_current_node = n;
+              break;
             }
-            return false;
+            keybuf[keybuf_len++] = ng_current_node->key;
+          }
+
+          for (int8_t i = keybuf_len - 1; i >= 0; i--) {
+            const naginata_node_t* n = ng_current_node->next_node(keybuf[i]);
+            if (n == NULL) {
+              break;
+            } else {
+              ng_current_node = n;
+            }
+          }
+
+        } else {
+          ng_current_node = next_node;
         }
-        default:
-            break;
+      } else {
+        if (ng_current_node->prev_node(keycode) != NULL) {
+          ng_send_string(ng_current_node->value);
+        }
+        const naginata_node_t* n = ng_current_node->prev_node(keycode);
+        if (n != NULL) {
+          ng_current_node = n;
+        }
+      }
+      return false;
     }
-    return true;
+    default:
+      break;
+  }
+  return true;
 }
