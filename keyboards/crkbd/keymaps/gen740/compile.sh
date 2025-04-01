083a2bc2a2e4d0a1b92836d1e362929b03a78c1d
gen740 <54583542+gen740@users.noreply.github.com>
2025-04-01 13:02:25 +0900

    chore(compile.sh): Rewrite compile.sh

--- keyboards/crkbd/keymaps/gen740/compile.sh
+++ keyboards/crkbd/keymaps/gen740/compile.sh
@@ -1,7 +1,5 @@
 #!bash
 
-# qmk generate-compilation-database -kb crkbd/rev4_1/standard -km gen740
-
 # flash
 if [ "$1" = "flash" ]; then
     echo "Flashing..."
@@ -12,6 +10,7 @@ if [ "$1" = "flash" ]; then
 # compile
 elif [ "$1" = "compile" ]; then
     echo "Compiling..."
+    qmk generate-compilation-database -kb crkbd/rev4_1/standard -km gen740
     python3 ./generator/code_generator.py dvorak
     python3 ./generator/code_generator.py naginata
     qmk compile -kb crkbd/rev4_1/standard -km gen740
